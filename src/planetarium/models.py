37e5f553098460c557cc524b504c1d80bc5716d4
4ikkamony <compbanderloga@gmail.com>
2025-02-21 18:41:26 +0200

    Update planetarium/models.py
    Add basic implementation of models I've decided to use.
    
    They are:
      - Dome
        Represents hall where shows take place.
        For now has properties: name, rows, seaths_in_row.
        I think I should add some property to represent address of a Dome.
        For now, I start simple.
    
      - Show
        provides general information about a show,
        its title, description and themes.
        A Show has one or more Event, which are actual planetarium sessions
        people buy tickets for.
    
      - Event
        Provides information about specific Event in planetarium.
        References a Show and a Dome, that provide info about location and
        content of the Event.
        Event itself specifies event time, and is refernced by Ticket.
    
      - Booking
        Represent Booking of Tickets by a User.
        Has created_at, time when it was created
        Refernces User, who requested the booking,
        References one or more Ticket, as Users may buy multiple tickets at
        a time
    
      - Ticket
        A ticket for an event.
        Has row and seat numbers that User chose to enjoy the Event at
        References Event, Booking and TicketType
    
      - TicketType
        Has additional info to help categorize the tickets.
        Has category and price. Category who this ticket is for, basically.
    
        Our Project Management Team(namely: me, myself and I + The Voices)
        still hasn't reached a consensus on what categories should the
        planetarium have. We have adult, student, child, wheelchair user(and
        free ticket for his companion, on demand)

--- src/planetarium/models.py
+++ src/planetarium/models.py
@@ -1,3 +1,77 @@
+from django.utils.translation import gettext_lazy as _
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Dome(models.Model):
+    """Location where shows take place"""
+
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+
+class ShowTheme(models.Model):
+    """Theme of the show"""
+
+    name = models.CharField(max_length=255)
+
+
+class Show(models.Model):
+    """General information about a planetarium show"""
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    show_themes = models.ManyToManyField(ShowTheme, related_name="astronomy_shows")
+
+
+class Event(models.Model):
+    """A specific planetarium event session in a specific Show"""
+
+    astronomy_show = models.ForeignKey(
+        Show, on_delete=models.CASCADE, related_name="events"
+    )
+    dome = models.ForeignKey(Dome, on_delete=models.CASCADE, related_name="events")
+    event_time = models.DateTimeField()
+
+
+class Booking(models.Model):
+    """Booking of one or more tickets by a user"""
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="bookings"
+    )
+
+
+class TicketType(models.Model):
+    """Different ticket categories and their prices"""
+
+    class TicketCategory(models.TextChoices):
+        """Ticket category options"""
+
+        ADULT = "adult", _("Adult")
+        STUDENT = "student", _("Student")
+        CHILD = "child", _("Child")
+        WHEELCHAIR_USER = "wheelchair", _("Wheelchair User")
+        COMPANION = "companion", _("Companion")
+
+    category = models.CharField(
+        max_length=20, choices=TicketCategory.choices, unique=True
+    )
+
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
+
+
+class Ticket(models.Model):
+    """A ticket for a specific Event"""
+
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="tickets")
+    booking = models.ForeignKey(
+        Booking, on_delete=models.CASCADE, related_name="tickets"
+    )
+    ticket_type = models.ForeignKey(
+        TicketType, on_delete=models.CASCADE, related_name="tickets"
+    )
