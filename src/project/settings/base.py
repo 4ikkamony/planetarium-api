94c9651000917cee1c20f6d6fb91be948354b440
4ikkamony <compbanderloga@gmail.com>
2025-02-21 18:35:35 +0200

    Fix code style

--- src/project/settings/base.py
+++ src/project/settings/base.py
@@ -20,10 +20,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
     "rest_framework",
     "drf_spectacular",
-
     "planetarium",
     "user",
 ]
