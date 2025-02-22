7cf042603f05f27273c68654f44e299ce7062c2f
4ikkamony <compbanderloga@gmail.com>
2025-02-22 18:51:18 +0200

    Fix shows_search return Response

--- src/planetarium/views.py
+++ src/planetarium/views.py
@@ -98,10 +98,6 @@ def shows_search(request):
         )
 
     serializer = ShowSearchSerializer(results, many=True)
-
-    if serializer.errors:
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 
