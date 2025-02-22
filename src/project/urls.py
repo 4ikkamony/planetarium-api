a8bf7f8ee5be84f3a8c1e733d1081f7cbf57dc13
4ikkamony <compbanderloga@gmail.com>
2025-02-22 14:57:38 +0200

    Update src/project/urls.py
    Include planetarium.urls

--- src/project/urls.py
+++ src/project/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path("api/planetarium/", include("planetarium.urls", namespace="planetarium")),
     path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
