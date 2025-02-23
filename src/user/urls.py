9abd403ca0e068f8e45a24afb888ef790d7371c7
4ikkamony <compbanderloga@gmail.com>
2025-02-23 21:03:22 +0200

    Update user urls and views
    user/views.py
      Set no permissions to CreateUserView. Anonimous users should be able
      to register
    
    user/urls.py
      add token/, token/refresh/ and token/verify/ endpoints
      that use rest_framework_simplejwt views

--- src/user/urls.py
+++ src/user/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 from user.views import CreateUserView, ManageUserView
 
 app_name = "user"
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="create"),
-    path("me/", ManageUserView.as_view(), name="manage"),
+    path("register/", CreateUserView.as_view(), name="user-create"),
+    path("me/", ManageUserView.as_view(), name="user-manage"),
+    path("token/", TokenObtainPairView.as_view(), name="token-obtain-pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token-verify"),
 ]
