778c4b9ce51049fea4bec44ef265677034891f14
4ikkamony <compbanderloga@gmail.com>
2025-02-24 13:34:40 +0200

    Update payment/views.py
    Add CheckoutSessionCreateView
      - Using booking id, creates new Stripe Checkout session
        and a Payment object with 'pending' status
    
    Add CheckoutSessionSuccessView
      - Change Payment object status to 'completed' when
        payment succeeds
    
    Add CheckoutSessionCancelView
      - Change Payment object status to 'cancelled' when
        payment gets cancelled

--- src/payment/views.py
+++ src/payment/views.py
@@ -8,9 +8,11 @@ from rest_framework.views import APIView
 from .models import Payment
 from planetarium.models import Booking
 
+
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
-class CreateCheckoutSession(APIView):
+
+class CheckoutSessionCreateView(APIView):
     permission_classes = ()
 
     def post(self, request):
@@ -19,12 +21,20 @@ class CreateCheckoutSession(APIView):
             return Response(
                 {"error": "Booking ID is required."}, status=status.HTTP_400_BAD_REQUEST
             )
-        booking = get_object_or_404(Booking, id=booking_id)
-        amount = booking.calculate_booking_price()
-        amount_cents = int(amount * 100)
 
-        success_url = request.build_absolute_uri(reverse("planetarium:stripe-success")) + "?session_id={CHECKOUT_SESSION_ID}"
-        cancel_url = request.build_absolute_uri(reverse("planetarium:stripe-cancel"))
+        booking = get_object_or_404(Booking, id=booking_id)
+
+        amount = booking.calculate_booking_price()
+        amount_kopiyok = int(amount * 100)
+
+        success_url = (
+            request.build_absolute_uri(reverse("payment:stripe-success"))
+            + "?session_id={CHECKOUT_SESSION_ID}"
+        )
+        cancel_url = (
+            request.build_absolute_uri(reverse("payment:stripe-cancel"))
+            + "?session_id={CHECKOUT_SESSION_ID}"
+        )
 
         try:
             session = stripe.checkout.Session.create(
@@ -36,7 +46,7 @@ class CreateCheckoutSession(APIView):
                             "product_data": {
                                 "name": f"Booking #{booking.id} of {booking.user}",
                             },
-                            "unit_amount": amount_cents,
+                            "unit_amount": amount_kopiyok,
                         },
                         "quantity": 1,
                     }
@@ -54,11 +64,13 @@ class CreateCheckoutSession(APIView):
             booking=booking,
             amount=amount,
             session_id=session.id,
+            session_url=session.url,
         )
 
-        return Response({"session_id": session.id})
+        return Response({"session_id": session.id, "session_url": session.url})
 
-class StripeSuccessAPI(APIView):
+
+class CheckoutSessionSuccessView(APIView):
     permission_classes = ()
 
     def get(self, request):
@@ -70,27 +82,50 @@ class StripeSuccessAPI(APIView):
         try:
             session = stripe.checkout.Session.retrieve(session_id)
             if session.payment_status == "paid":
-                payment = Payment.objects.filter(session_id=session_id).first()
+                payment = get_object_or_404(Payment, session_id=session.id)
                 if payment:
                     payment.status = "completed"
                     payment.save()
                     return Response(
-                        {"message": "Payment successful", "booking_id": payment.booking.id}
+                        {
+                            "message": "Payment successful",
+                            "booking_id": payment.booking.id,
+                        }
                     )
 
                 return Response(
                     {"error": "Booking not found"}, status=status.HTTP_404_NOT_FOUND
                 )
 
-            return Response({"error": "Payment not completed"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"error": "Payment not completed"}, status=status.HTTP_400_BAD_REQUEST
+            )
 
         except stripe.error.StripeError as e:
             return Response(
                 {"error": f"Stripe error: {str(e)}"}, status=status.HTTP_400_BAD_REQUEST
             )
 
-class StripeCancelAPI(APIView):
+
+class CheckoutSessionCancelView(APIView):
     permission_classes = ()
 
     def get(self, request):
-        return Response({"message": "Payment was cancelled. You can try again."})
+        session_id = request.GET.get("session_id")
+        try:
+            session = stripe.checkout.Session.retrieve(session_id)
+        except stripe.error.StripeError as e:
+            return Response(
+                {"error": f"Stripe error: {str(e)}"}, status=status.HTTP_400_BAD_REQUEST
+            )
+        if session:
+            payment = get_object_or_404(Payment, session_id=session.id)
+            payment.status = "cancelled"
+            payment.save()
+        return Response(
+            {
+                "message": "Payment was cancelled. You can try again.",
+                "session_id": session.id,
+                "session_url": session.url,
+            }
+        )
