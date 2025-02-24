2f2035df2a33fe16a96edf90ab742923ad2e4e00
4ikkamony <compbanderloga@gmail.com>
2025-02-24 13:41:16 +0200

    Add routing for stripe checkout
    Add endpoints:
      - 'stripe/checkout/create/'
      - 'stripe/checkout/success/'
      - 'stripe/checkout/cancel/'

--- src/payment/urls.py
+++ src/payment/urls.py
@@ -1,15 +1,20 @@
 from django.urls import path
 
 from payment.views import (
-    CreateCheckoutSession,
-    StripeSuccessAPI,
-    StripeCancelAPI
+    CheckoutSessionCreateView,
+    CheckoutSessionSuccessView,
+    CheckoutSessionCancelView,
 )
 
+
 urlpatterns = [
-    path("stripe/checkout/", CreateCheckoutSession.as_view(), name="stripe-checkout"),
-    path("stripe/success/", StripeSuccessAPI.as_view(), name="stripe-success"),
-    path("stripe/cancel/", StripeCancelAPI.as_view(), name="stripe-cancel")
+    path(
+        "stripe/checkout/create/", CheckoutSessionCreateView.as_view(), name="stripe-checkout"
+    ),
+    path(
+        "stripe/checkout/success/", CheckoutSessionSuccessView.as_view(), name="stripe-success"
+    ),
+    path("stripe/checkout/cancel/", CheckoutSessionCancelView.as_view(), name="stripe-cancel"),
 ]
 
 app_name = "payment"
