8256db010e03793c9a5fedff8d0e7afbacc2ae13
4ikkamony <compbanderloga@gmail.com>
2025-02-24 13:25:31 +0200

    Update Paymend model
    Add Cancelled status
    Add session_url field, so that it can be later
    displayed on Booking list

--- src/payment/models.py
+++ src/payment/models.py
@@ -6,15 +6,21 @@ class Payment(models.Model):
     class Status(models.TextChoices):
         PENDING = "pending", _("Pending")
         COMPLETED = "completed", _("Completed")
-        FAILED = "failed", _("Failed")
+        CANCELLED = "cancelled", _("Cancelled")
 
     amount = models.DecimalField(max_digits=6, decimal_places=2)
-    status = models.CharField(max_length=10, choices=Status.choices, default=Status.PENDING)
+    status = models.CharField(
+        max_length=10, choices=Status.choices, default=Status.PENDING
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     session_id = models.CharField(max_length=255)
+    session_url = models.URLField(max_length=510)
     booking = models.OneToOneField(
-        "planetarium.Booking", on_delete=models.SET_NULL,
-        null=True, blank=True, related_name="payment"
+        "planetarium.Booking",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="payment",
     )
 
     def __str__(self):
