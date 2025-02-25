645869a952d76ba80b5996e41bd6f9917286e0cc
4ikkamony <compbanderloga@gmail.com>
2025-02-25 23:26:31 +0200

    Update base.py

--- src/project/settings/base.py
+++ src/project/settings/base.py
@@ -124,8 +124,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 REST_FRAMEWORK = {
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 5,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.BrowsableAPIRenderer",
